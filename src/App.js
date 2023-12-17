2b9bbc7a6d91c8b1fcceea7e31d14aa112152d1d
konpeki <92644639+konpeki02@users.noreply.github.com>
2023-12-16 20:59:28 -0600

    added new icons and link

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [numPlayers, setNumPlayers] = useState(2);
+  const [players, setPlayers] = useState(Array.from({ length: 2 }, (_, index) => `Player ${index + 1}`));
+  const [teamResults, setTeamResults] = useState({ team1: [], team2: [] });
+
+  const updatePlayers = (event) => {
+    const newNumPlayers = parseInt(event.target.value, 10);
+    setNumPlayers(newNumPlayers);
+    setPlayers(Array.from({ length: newNumPlayers }, (_, index) => `Player ${index + 1}`));
+  };
+
+  const handlePlayerChange = (index, value) => {
+    const updatedPlayers = [...players];
+    updatedPlayers[index] = value;
+    setPlayers(updatedPlayers);
+  };
+
+  const randomizeTeams = () => {
+    const randomizedPlayers = [...players].sort(() => Math.random() - 0.5);
+    const teamSize = numPlayers % 2 === 0 ? numPlayers / 2 : Math.ceil(numPlayers / 2);
+
+    const team1 = randomizedPlayers.slice(0, teamSize);
+    const team2 = randomizedPlayers.slice(teamSize);
+
+    setTeamResults({ team1, team2 });
+  };
+
+    return (
+    <div className="container">
+      <label htmlFor="numPlayers">Number of Players:</label>
+      <input
+        type="number"
+        id="numPlayers"
+        min="2"
+        value={numPlayers}
+        onChange={updatePlayers}
+      />
+
+      <div id="playersContainer">
+        {players.map((player, index) => (
+          <input
+            key={index}
+            type="text"
+            placeholder={`Player ${index + 1}`}
+            value={player}
+            onChange={(e) => handlePlayerChange(index, e.target.value)}
+          />
+        ))}
+      </div>
+
+      <button onClick={randomizeTeams}>Randomize Teams</button>
+
+      <div id="teamResults">
+        <p>Team 1: {teamResults.team1.join(", ")}</p>
+        <p>Team 2: {teamResults.team2.join(", ")}</p>
+      </div>
+
+      <div className="contact-info">
+        <a href="https://hashtagbasketball.com/" target="_blank" rel="noopener noreferrer"># Basketball</a>
+        <a href="https://github.com/konpeki02" target="_blank" rel="noopener noreferrer">Github</a>
+        <a href="https://www.nba.com/fantasy" target="_blank" rel="noopener noreferrer">Fantasy</a>
+      </div>
+
+
+      <div className="signature">
+        <p>Made by Tony </p>
+      </div>
     </div>
   );
 }
